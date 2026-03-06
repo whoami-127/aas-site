96a8408b895534a25e4362790db4b8c98cadb569
vubinh127 <vubinh120720@gmail.com>
2026-03-06 23:26:26 +0700

    Add slide

--- js/main.js
+++ js/main.js
@@ -144,3 +144,23 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
 });
+
+var swiper = new Swiper(".reviews-slider", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  }
+});
